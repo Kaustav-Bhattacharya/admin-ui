fbbfd1dae163295b4242c092bb8510c431a7d039
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2023-09-08 22:35:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { Menu } from "./components/menu/Menu"
+import { NavBar } from "./components/navbar/NavBar"
+import { Users } from "./pages/users/Users"
+import { Home } from "./pages/home/Home"
+import { Products } from "./pages/products/Products"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
+import { Footer } from "./components/footer/Footer"
+import "./styles/global.scss"
+
+function App() {
+  // if we have another page that doesn't needs to be display the navbar and other components
+  // we can declare path to that component and not include the layopout with it
+  const Layout = () => {
+    return (
+      <div className="main">
+        <NavBar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,14 @@
+import './navbar.scss'
+
+const NavBar = () => {
+  return <div className="navbar">
+    <div className="logo">
+      <img src="logo.svg" alt="logo" />
+      <span>K-Admin</span>
+    </div>
+    <div className="icons"></div>
+  </div>
+
+}
+
+export { NavBar }
