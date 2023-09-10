525e5d670833e18f17f9a9c13abf2c03c70c6f65
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2023-09-11 01:02:02 +0530

    bar chart and line chart added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,14 @@
+import { BarChartBox } from "../../components/barChartBox/BarChartBox"
 import { Chartbox } from "../../components/chartbox/Chartbox"
 import { TopBox } from "../../components/topBox/TopBox"
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data"
 import "./home.scss"
 
 const Home = () => {
@@ -9,21 +18,25 @@ const Home = () => {
         <TopBox />
       </div>
       <div className="box box2">
-        <Chartbox />
+        <Chartbox {...chartBoxUser} />
       </div>
       <div className="box box3">
-        <Chartbox />
+        <Chartbox {...chartBoxProduct} />
       </div>
-      <div className="box box4">
-        <Chartbox />
-      </div>
+      <div className="box box4">BOX 4</div>
       <div className="box box5">
-        <Chartbox />
+        <Chartbox {...chartBoxConversion} />
       </div>
-      <div className="box box6">BOX 6</div>
+      <div className="box box6">
+        <Chartbox {...chartBoxRevenue} />
+      </div>
       <div className="box box7">BOX 7</div>
-      <div className="box box8">BOX 8</div>
-      <div className="box box9">BOX 9</div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit}/>
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue}/>
+      </div>
     </div>
   )
 }
