b0cd61ea298ee8b0d7e47e3112807cee0aa91b7f
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2023-09-12 23:26:59 +0530

    usewr adding modal created

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,40 @@
+import { GridColDef } from "@mui/x-data-grid"
+import "./add.scss"
+
+type Props = {
+  path: string
+  columns: GridColDef[]
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Add = (props: Props) => {
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+e.preventDefault()
+
+//add user to the db 
+console.log(e , " has been added to the store")
+    }
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={()=>props.setOpen(false
+            )}>X</span>
+        <h1>Add new {props.path}</h1>
+        <form>
+          {props.columns
+            .filter((item) => item.field !== "img" || item.field !== "img")
+            .map((column) => (
+              <div className="items">
+                <label> {column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+            <button>Send</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export { Add }
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,56 +1,69 @@
+import {useState} from 'react'
 import { DataTable } from "../../components/dataTable/DataTable"
 import { userRows } from "../../data"
 import "./users.scss"
-import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid"
+import { GridColDef } from "@mui/x-data-grid"
+import { Add } from '../../components/add/Add'
 
-const columns: GridColDef[] = [
+const columns: GridColDef[] =[
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "avatar",
+    field: "img",
     headerName: "Avatar",
     width: 100,
     renderCell: (params) => {
-      return <img src={params.row.img || "/noavatar.png"} alt="" />
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
     },
   },
   {
     field: "firstName",
+    type: "string",
     headerName: "First name",
     width: 150,
-    editable: true,
   },
   {
     field: "lastName",
+    type: "string",
     headerName: "Last name",
     width: 150,
-    editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
   },
-]
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
+const[openModal, setOpenModal] = useState(false)
+  
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={()=>setOpenModal(true)}>Add New User</button>
       </div>
       <DataTable path="users" columns={columns} rows={userRows} />
+      {openModal && <Add path='user' columns={columns} setOpen={setOpenModal}/>}
     </div>
   )
 }
